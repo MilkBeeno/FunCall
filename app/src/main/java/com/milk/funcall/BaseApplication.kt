b1b3eec82fd521f3a1f2d45c206c3af1a028e951
MilkBeeno <milkbeeno@gmail.com>
2022-06-16 16:53:33 +0800

    调整库初始化结构

--- app/src/main/java/com/milk/funcall/BaseApplication.kt
+++ app/src/main/java/com/milk/funcall/BaseApplication.kt
@@ -1,10 +1,8 @@
 package com.milk.funcall
 
 import android.app.Application
-import android.util.Log
 import com.milk.funcall.common.mdr.DataBaseManager
 import com.milk.simple.ktx.ioScope
-import com.milk.simple.log.LogTree
 import com.milk.simple.log.Logger
 import com.milk.simple.mdr.KvManger
 
@@ -21,18 +19,9 @@ class BaseApplication : Application() {
 
     private fun initializeLibrary() {
         ioScope {
-            initializeLogger()
             KvManger.initialize(instance)
+            Logger.initialize(BuildConfig.DEBUG)
             DataBaseManager.initialize(instance)
         }
-    }
-
-    private fun initializeLogger() {
-        val debug = BuildConfig.DEBUG || Log.isLoggable("MyLog", Log.DEBUG)
-        Logger.initialize(debug, tree = object : LogTree {
-            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                // 将错误日历写在文件中
-            }
-        })
     }
 }
