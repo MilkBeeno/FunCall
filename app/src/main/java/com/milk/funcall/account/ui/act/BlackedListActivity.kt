7898e73ab1249cf3f09675b4f99294795f77a839
MilkBeeno <milkbeeno@gmail.com>
2022-07-05 15:15:04 +0800

    黑名单列表功能

--- app/src/main/java/com/milk/funcall/account/ui/act/BlackedListActivity.kt
+++ app/src/main/java/com/milk/funcall/account/ui/act/BlackedListActivity.kt
@@ -3,6 +3,7 @@ package com.milk.funcall.account.ui.act
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.milk.funcall.R
 import com.milk.funcall.common.ui.AbstractActivity
 import com.milk.funcall.databinding.ActivityBlackedListBinding
 import com.milk.simple.ktx.viewBinding
@@ -12,6 +13,12 @@ class BlackedListActivity : AbstractActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initializeView()
+    }
+
+    private fun initializeView() {
+        binding.headerToolbar.showArrowBack()
+        binding.headerToolbar.setTitle(R.string.mine_blacked_list)
     }
 
     companion object {
