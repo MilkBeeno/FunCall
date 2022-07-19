fa89fcb99af0847eac525b7fdf39205e6d7400d5
MilkBeeno <milkbeeno@gmail.com>
2022-07-19 17:39:48 +0800

    删除无用文件

--- app/src/main/java/com/milk/funcall/common/media/loader/Gif.kt
+++
@@ -1,9 +0,0 @@
-package com.milk.funcall.common.media.loader
-
-//.components {
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//        add(ImageDecoderDecoder.Factory())
-//    } else {
-//        add(GifDecoder.Factory())
-//    }
-//}
--- app/src/main/java/com/milk/funcall/common/media/loader/Svg.kt
+++
@@ -1,5 +0,0 @@
-package com.milk.funcall.common.media.loader
-
-//.components {
-//    add(SvgDecoder.Factory())
-//}
