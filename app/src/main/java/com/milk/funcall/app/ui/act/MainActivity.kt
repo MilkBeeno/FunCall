a68df416ef48d3bea5be45c4042921473b0e204b
MilkBeeno <milkbeeno@gmail.com>
2022-07-19 20:16:00 +0800

    私聊页面心跳包实现

--- app/src/main/java/com/milk/funcall/app/ui/act/MainActivity.kt
+++ app/src/main/java/com/milk/funcall/app/ui/act/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.milk.funcall.app.ui.act
 
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.jeremyliao.liveeventbus.LiveEventBus
 import com.milk.funcall.account.ui.frag.MineFragment
+import com.milk.funcall.app.MainService
 import com.milk.funcall.app.ui.view.BottomNavigation
 import com.milk.funcall.chat.ui.frag.ChatMessageFragment
 import com.milk.funcall.common.constrant.EventKey
@@ -17,6 +21,8 @@ import com.milk.simple.ktx.immersiveStatusBar
 import com.milk.simple.ktx.navigationBarPadding
 import com.milk.simple.ktx.statusBarPadding
 import com.milk.simple.ktx.viewBinding
+import com.milk.simple.log.Logger
+import java.util.*
 
 class MainActivity : AbstractActivity() {
     private val binding by viewBinding<ActivityMainBinding>()
@@ -24,11 +30,14 @@ class MainActivity : AbstractActivity() {
     private val homeFragment = HomeFragment.create()
     private val messageFragment = ChatMessageFragment.create()
     private val mineFragment = MineFragment.create()
+    private lateinit var serviceIntent: Intent
+    private lateinit var connection: ServiceConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initializeView()
+        initializeService()
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -59,6 +68,33 @@ class MainActivity : AbstractActivity() {
                 }
             }
         }
+    }
+
+    private fun initializeService() {
+        connection = object : ServiceConnection {
+            override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+                // 和服务绑定成功后，服务会回调该方法
+                // 服务异常中断后重启，也会重新调用改方法
+                // Logger.d("服务初始化完成", "IM-Service")
+                val timerTask = object : TimerTask() {
+                    override fun run() {
+                        Logger.d(
+                            "IM Okhttp 心跳包 当前时间=${System.currentTimeMillis()}",
+                            "IM-Service"
+                        )
+                    }
+                }
+                Timer().schedule(timerTask, 0, 10000)
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                // 当服务异常终止时会调用
+                // 注意，unbindService时不会调用
+                // Logger.d("服务异常终止了", "IM-Service")
+            }
+        }
+        serviceIntent = Intent(this, MainService::class.java)
+        bindService(serviceIntent, connection, BIND_AUTO_CREATE)
     }
 
     private fun setTabSelection(fragment: Fragment) {
@@ -94,6 +130,11 @@ class MainActivity : AbstractActivity() {
         transaction.hide(homeFragment)
         transaction.hide(messageFragment)
         transaction.hide(mineFragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(connection)
     }
 
     override fun onInterceptKeyDownEvent(): Boolean = true
