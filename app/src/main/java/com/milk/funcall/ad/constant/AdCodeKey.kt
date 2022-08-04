81af1b82e3a8d4c35ab324a7894beb69208331bf
MilkBeeno <milkbeeno@gmail.com>
2022-08-04 11:39:51 +0800

    修复广告配置拉取不正确问题

--- app/src/main/java/com/milk/funcall/ad/constant/AdCodeKey.kt
+++ app/src/main/java/com/milk/funcall/ad/constant/AdCodeKey.kt
@@ -31,29 +31,29 @@ object AdCodeKey {
     /*******************************  以下是广告开关配置保存的 KEY ************************************/
 
     /** 广告总开关 */
-    internal const val AD_MASTER_SWITCH = "Ad master switch"
+    internal const val AD_MASTER_SWITCH = "Ad_master_switch"
 
     /** 启动页面广告是否开启 */
-    internal const val APP_LAUNCH_INTERSTITIAL = "app launch interstitial"
+    internal const val APP_LAUNCH_INTERSTITIAL = "app_launch_interstitial"
 
     /** 首页瀑布流广告一是否开启 */
-    internal const val HOMEPAGE_WATERFALL_ADS_FIRST = "Homepage waterfall Ads-1"
+    internal const val HOMEPAGE_WATERFALL_ADS_FIRST = "Homepage_waterfall_Ads_1"
 
     /** 首页瀑布流广告一是否开启 */
-    internal const val HOMEPAGE_WATERFALL_ADS_SECOND = "Homepage Waterfall  Ads-2"
+    internal const val HOMEPAGE_WATERFALL_ADS_SECOND = "Homepage_Waterfall_Ads_2"
 
     /** 首页瀑布流广告一是否开启 */
-    internal const val HOMEPAGE_WATERFALL_ADS_THIRD = "Homepage Waterfall  Ads-3"
+    internal const val HOMEPAGE_WATERFALL_ADS_THIRD = "Homepage_Waterfall_Ads_3"
 
     /** 底部横幅广告是否开启 */
-    internal const val BANNER_OF_HOMEPAGE = "Banner of homepage"
+    internal const val BANNER_OF_HOMEPAGE = "Banner_of_homepage"
 
     /** 个人主页查看用户联系方式激励视频广告是否开启 */
-    internal const val VIEW_CONTACT_VIDEO_ADS = "View Contact Video Ads"
+    internal const val VIEW_CONTACT_VIDEO_ADS = "View_Contact_Video_Ads"
 
     /** 个人主页查看用户视频激励视频广告是否开启 */
-    internal const val VIEW_USER_VIDEO_ADS = "View User Video Ads"
+    internal const val VIEW_USER_VIDEO_ADS = "View_User_Video_Ads"
 
     /** 个人主页查看用户图片激励视频广告是否开启 */
-    internal const val VIEW_PHOTO_ADS = "view photo Ads"
+    internal const val VIEW_PHOTO_ADS = "view_photo_Ads"
 }
