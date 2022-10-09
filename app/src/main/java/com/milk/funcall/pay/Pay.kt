5c0e4441ba75872058a9803dd8baf258f43faa8c
MilkBeeno <milkbeeno@gmail.com>
2022-10-09 15:40:59 +0800

    谷歌支付SDK接入

---
+++ app/src/main/java/com/milk/funcall/pay/Pay.kt
@@ -0,0 +1,27 @@
+package com.milk.funcall.pay
+
+import android.app.Activity
+import android.content.Context
+
+interface Pay {
+    /** SDK 初始化 */
+    fun initialize(context: Context)
+
+    /** 支付断开连接 */
+    fun disconnect()
+
+    /** 支付连接成功 */
+    fun connected()
+
+    /** 进行购买 */
+    fun launchPurchase(activity: Activity, productDetails: Any?)
+
+    /** 购买成功 */
+    fun purchaseSuccess(purchaseToken:Any)
+
+    /** 取消购买 */
+    fun purchaseCancel()
+
+    /** 购买失败 */
+    fun purchaseFailure()
+}
